05d712793b256eaabcbaf2da3a56e3cbda72a558
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-12-12 12:11:24 -0800

    Tree problems

--- src/week3_assignment/BinaryTree.java
+++ src/week3_assignment/BinaryTree.java
@@ -18,5 +18,19 @@ public class BinaryTree {
 		return v & isSameTree(A.left, B.left) & isSameTree(A.right, B.right);
 	}
 	
-	// Problem 2 : 
+	// Problem 2 : Max Depth of binary tree
+	
+	public int maxDepth(TreeNode A) {
+        if (A == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(maxDepth(A.left), maxDepth(A.right));
+        }
+    }
+	
+	// Problem 3 : Inorder traversal (without recursion)
+	
+	public int[] inorderTraversal(TreeNode A)  {
+		return null;
+	}
 }
