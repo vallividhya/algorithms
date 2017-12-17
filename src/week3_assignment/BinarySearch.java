0c8ab77b267a6fe5676dda3a4020304482a2dc1a
Valli Vidhya Venkatesan <vallividhyavenkatesan@gmail.com>
2017-12-16 16:44:38 -0800

    Binary trees and binary search

--- src/week3_assignment/BinarySearch.java
+++ src/week3_assignment/BinarySearch.java
@@ -1,6 +1,7 @@
 package week3_assignment;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BinarySearch {
@@ -70,4 +71,104 @@ public class BinarySearch {
         }
         return -1;
     }
+	
+	
+	// Problem 3 : Square root of a number
+	
+		public int sqrt(int a) {
+	    if (a == 0 || a == 1) 
+	        return a;
+	        
+	    int low = 1;
+		    int high = a / 2;
+		    while (low <= high) {
+		        int mid = low + (high - low) / 2;
+		        if (mid > a / mid) {
+	            high = mid - 1;
+	        }
+	        else if (mid + 1 <= a / (mid + 1)) {
+	            low  = mid + 1;
+	        }
+	        else {
+	            return mid;
+	        }
+	    }
+	    return -1;
+	}
+		
+		// Problem : 4 Power
+		
+		public int pow(int x, int n, int d) {
+			   
+	        long a = x;
+	        long res = 1L;
+	    
+	        while (n > 0) {
+	          if (n % 2 == 1) {
+	            res *= a;
+	            res %= d;
+	          }
+	    
+	          a *= a;
+	          a %= d;
+	          n = n >> 1;
+	        }
+	    
+	        res = (res + d) % d;
+	    
+	        return (int) res; 	
+		}
+		
+		
+	// Count for each value in maxes, how many numbers are less than or equal to the value in maxes
+	// Return result as array of counts for each maxes value. 
+	
+    static int[] counts(int[] nums, int[] maxes) {
+        int[] result = new int[maxes.length];
+        Arrays.sort(nums);
+        
+       
+        for (int i = 0; i < maxes.length; i++) {
+        	 int low = 0;
+             int high = nums.length - 1;
+             int target = maxes[i] + 1;
+            // target is each val in maxes + 1;
+            while (low < high) {
+                int mid = (low + high) / 2;
+                if (nums[mid] < target) {
+                    low = mid + 1;
+                } else {
+                    high = mid;
+                }
+            }
+            if (nums[low] < target) {
+                result[i] = nums.length;
+            } else if (nums[low] >= target) {
+                if ((low - 1) >= 0) {
+                    result[i] = low;
+                } else {
+                    result[i] = 0;
+                }
+            }
+            
+        }
+        for (Integer i : result) {
+        	System.out.print(i + " ");
+        }
+        return result;
+    }
+    
+	public static void main(String[] args) {
+		//System.out.println(pow(-1, 1, 20));
+		//System.out.println(pow(0, 0, 1));
+		//System.out.println(pow(5, 55, 221));
+//		System.out.println(pow(2, 5, 2));
+//		System.out.println(pow(71045970,41535484, 64735492));
+		//System.out.println(1 % 1);
+//		int[] a = {1, 4, 2, 4};
+//		int[] b = {3, 5};
+		int[] a = {2, 10, 5, 4, 8};
+		int[] b = {3, 1, 7, 8};
+		counts(a, b);
+	}
 }
--- src/week3_assignment/BinaryTree.java
+++ src/week3_assignment/BinaryTree.java
@@ -1,6 +1,13 @@
 package week3_assignment;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+
 public class BinaryTree {
+	
 
 	// Problem 1: Identical Binary Trees
 	
@@ -31,6 +38,174 @@ public class BinaryTree {
 	// Problem 3 : Inorder traversal (without recursion)
 	
 	public int[] inorderTraversal(TreeNode A)  {
-		return null;
+		if (A == null) {
+			return null;
+		}
+		
+		ArrayList<Integer> resultList = new ArrayList<Integer>();
+		Stack<TreeNode> stack = new Stack<>();
+		TreeNode current = A;
+		while (current != null) {
+			stack.push(current);
+			current = current.left;
+		}
+		
+		while (!stack.isEmpty()) {
+			
+			TreeNode n = stack.pop();
+			resultList.add(n.val);
+			if (n.right != null) {
+				n = n.right;
+			}
+			while (n != null) {
+				stack.push(n);
+				n = n.left;
+			}
+			
+		}
+		int[] result = new int[resultList.size()];
+		for (int i = 0; i < resultList.size(); i++) {
+			result[i] = resultList.get(i);
+		}
+		return result;
+	}
+	
+	
+	// Problem : 4 - Invert Tree
+	
+	public TreeNode invertTree(TreeNode A) {
+        if (A == null) {
+            return A;
+        } else {
+            TreeNode temp = A.left;
+            A.left = A.right;
+            A.right= temp;
+            invertTree(A.left);
+            invertTree(A.right);
+            return A;
+        }
+       
+    }
+	
+	// Problem: 5 - Path Sum
+	
+	public int hasPathSum(TreeNode A, int B) {
+        if (A == null) {
+            return 0;
+        } else if (A != null && A.left == null && A.right == null) {
+           return A.val == B ? 1 : 0; 
+        } else {
+            int diff = B - A.val;
+            int leftVal = hasPathSum(A.left, diff);
+            int rightVal = hasPathSum(A.right, diff);
+            return leftVal | rightVal;
+        }
+    }
+	
+	//Problem: 6 - Symmetric Binary Tree
+	
+	public int isSymmetric(TreeNode A) {
+        if (A == null) {
+            return 1;
+        }
+        if (A.left == null && A.right != null) {
+            return 0;
+        }
+        if (A.right == null && A.left != null) {
+            return 0;
+        }
+        if (A.left != null && A.right != null) {
+            return A.left.val == A.right.val? 1 : 0;
+        } 
+        return isSymmetric(A.left) & isSymmetric(A.right);
+    }
+	
+	public boolean hasPathSumBFS(TreeNode root, int sum) {
+		 Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		 Queue<Integer> sumQ = new LinkedList<Integer>();
+			
+			if (root != null) {
+				queue.add(root);
+				sumQ.add(root.val);
+			}
+			
+			while (!queue.isEmpty()) {
+				TreeNode n = queue.poll();
+				int sumInQ = sumQ.poll();
+				if (n.left == null && n.right == null && sumInQ == sum) {
+					return true;
+				}
+				if (n.left != null) {
+					queue.add(n.left);
+					sumQ.add(sumInQ + n.left.val);
+				}
+				if (n.right != null) {
+					queue.add(n.right);
+					sumQ.add(sumInQ + n.right.val);
+				}
+				
+			}
+			return false;
+	}
+	
+	// Insert a new node in the tree, given the root of the tree and return the root of the tree
+		public TreeNode insert(TreeNode node, int val) {
+			if (node == null) {
+				node = new TreeNode(val);
+			} else {
+				if (val < node.val) {
+					node.left = insert(node.left, val);
+				} else {
+					node.right = insert(node.right, val);
+				}
+			}
+			return node;
+		}
+	
+		public void printTreePreOrder(TreeNode root) {
+			if (root == null) {
+				return;
+			} else {
+				System.out.print(root.val + " ");
+				printTreePreOrder(root.left);
+				printTreePreOrder(root.right);
+				
+			}
+		}
+		
+		public void printBFS(TreeNode root) {
+			if (root == null) {
+				return;
+			} else {
+				Queue<TreeNode> queue = new LinkedList<TreeNode>();
+				queue.add(root);
+				
+				while (!queue.isEmpty()) {
+					TreeNode node = queue.poll();
+					System.out.print(node.val + " ");
+					if (node.left != null) {
+						queue.add(node.left);	
+					} 
+					if (node.right != null) {
+						queue.add(node.right);
+					}	
+				}
+			}
+		}
+		
+	public static void main(String[] args) {
+		BinaryTree bst = new BinaryTree();
+		TreeNode root = bst.insert(null, 4);
+		System.out.println(root.val);
+		bst.insert(root, 2);
+		bst.insert(root, 12);
+		bst.insert(root, 1);
+		
+		bst.insert(root, 3);
+		bst.insert(root, 7);
+		bst.insert(root, 9);
+		bst.insert(root, 15);
+		bst.printBFS(root);
+		System.out.println("Has path sum 9? " + bst.hasPathSumBFS(root, 9));
 	}
 }
